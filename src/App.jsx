ed3ad6578c5c5b24f1330e273917bf457601394c
Jobayer Rahman <jobayerrahman.diu@gmail.com>
2025-11-24 00:21:06 +0600

    product list and product details page added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import ReactDOM from "react-dom/client";
+// @ts-ignore
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import BlogPage from "./pages/BlogPage";
+import ProductDetailsPage from "pages/ProductDetailsPage";
+import ProductListPage from "pages/ProductListPage";
 
 function App() {
 
@@ -10,6 +13,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/product-list" element={<ProductListPage />} />
+        <Route path="/product-details" element={<ProductDetailsPage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+// @ts-ignore
 import { defineConfig } from 'vite'
+// @ts-ignore
 import react from '@vitejs/plugin-react'
+// @ts-ignore
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
