ca85883cbfd980df994adc37a7bad626e5a3968b
Jobayer Rahman <jobayerrahman.diu@gmail.com>
2025-11-26 01:38:26 +0600

    TopHeader part added

--- src/header/TopHeader.jsx
+++ src/header/TopHeader.jsx
@@ -1,8 +1,26 @@
+// @ts-nocheck
 import React from 'react'
+import Container from '../components/commonLayouts/Container'
+import { Link } from 'react-router-dom'
+import { IoLocationOutline } from "react-icons/io5";
+import { MdOutlineLocalPhone } from "react-icons/md";
 
 const TopHeader = () => {
   return (
-    <div>TopHeader</div>
+    <div className='border-b border-[#BFBFBF] border-solid py-[22px]'>
+      <Container>
+        <div className='flex justify-between items-center'>
+          <div className='flex  items-center gap-[50px] font-normal text-sm relative after:content-[""] after:absolute  after:w-px after:h-8 
+          after:bg-[#BFBFBF] after:left-[60%] after:top-[20%] after: -translate-y-1/2'>
+            <Link to="https://maps.app.goo.gl/LG93jHEoGXTkYwzS9" target='_blank' className="flex items-center gap-2" >
+              <IoLocationOutline /> Notun Bazar,Badda,Dhaka 
+             </Link>
+            <Link to="tel:01707607518" className="flex items-center gap-2"> <MdOutlineLocalPhone /> 01707-607518 </Link>
+          </div>
+          <div>right</div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
