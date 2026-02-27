f71e354f766945b7f0852612f87a40e144a5ecfd
Jobayer Rahman <jobayerrahman.diu@gmail.com>
2026-02-27 22:18:29 +0600

    start again

--- src/header/TopHeader.jsx
+++ src/header/TopHeader.jsx
@@ -5,22 +5,12 @@ import { Link } from 'react-router-dom'
 import { IoLocationOutline } from "react-icons/io5";
 import { MdOutlineLocalPhone } from "react-icons/md";
 
+
 const TopHeader = () => {
   return (
-    <div className='border-b border-[#BFBFBF] border-solid py-[22px]'>
-      <Container >
-        <div className='flex justify-between items-center'>
-          <div className='flex items-center gap-[50px] font-normal text-sm relative after:content-[""] after:absolute after:w-px after:h-8 after:bg-[#BFBFBF] 
-          after:left-[62%] after:top-[50%] after:-translate-y-1/2'>
-            <Link to="https://maps.app.goo.gl/LG93jHEoGXTkYwzS9" target='_blank' className="flex items-center gap-2" >
-              <IoLocationOutline /> <span>Notun Bazar,Badda,Dhaka</span> 
-             </Link>
-            <Link to="tel:01707607518" className="flex items-center gap-2"> <MdOutlineLocalPhone /> <span>01707-607518</span> </Link>
-          </div>
-          <div></div>
-        </div>
-      </Container>
-    </div>
+    <>
+    <h1>Hello World</h1>
+    </>
   )
 }
 
